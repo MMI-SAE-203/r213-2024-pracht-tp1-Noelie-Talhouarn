4b7870cd4b6aead021c92d2138a77e389e856b98
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-04-11 16:20:45 +0200

    html accordeon

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -6,4 +6,16 @@ import { ref } from 'vue';
 
 <template>
   <h1>Accordéon</h1>
+  `<section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam corporis et veritatis harum ullam officia, optio vel mollitia nostrum, eveniet quidem tempore, dolorum nihil voluptatem expedita rerum eius inventore beatae.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Consectetur numquam sapiente natus et ratione ad. Reprehenderit accusamus necessitatibus architecto laboriosam ex voluptatum officiis eveniet, distinctio, tenetur sit explicabo. Deleniti veritatis unde recusandae provident consectetur fugiat rerum ut eius.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Facilis, accusamus. Veniam ratione debitis repudiandae accusamus quo. Alias, placeat. Quis repellat dicta ut ea consequuntur! Laboriosam, tenetur eos nostrum temporibus a officia optio enim facilis quisquam consequuntur veritatis ipsa.</p>
+  </section>
 </template>
