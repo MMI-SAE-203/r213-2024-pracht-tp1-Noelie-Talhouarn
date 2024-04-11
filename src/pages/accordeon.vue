d7a58eca0aada547bfb0add2672cd28dc0147326
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-04-11 16:34:00 +0200

    ajout d'éléments dans l'HTML

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-
+const sectionOpen = ref(1)
 
 </script>
 
 <template>
   <h1>Accordéon</h1>
-  `<section>
-    <button class="text-xl">bouton 1</button>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam corporis et veritatis harum ullam officia, optio vel mollitia nostrum, eveniet quidem tempore, dolorum nihil voluptatem expedita rerum eius inventore beatae.</p>
+  <section>
+    <button class="text-xl" @pointerdown="sectionOpen = 1">bouton 1</button>
+    <p v-show="sectionOpen === 1 ">loren...</p>
   </section>
   <section>
-    <button class="text-xl">bouton 2</button>
-    <p>Consectetur numquam sapiente natus et ratione ad. Reprehenderit accusamus necessitatibus architecto laboriosam ex voluptatum officiis eveniet, distinctio, tenetur sit explicabo. Deleniti veritatis unde recusandae provident consectetur fugiat rerum ut eius.</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 2">bouton 2</button>
+    <p v-show="sectionOpen === 2 ">loren...</p>
   </section>
   <section>
-    <button class="text-xl">bouton 3</button>
-    <p>Facilis, accusamus. Veniam ratione debitis repudiandae accusamus quo. Alias, placeat. Quis repellat dicta ut ea consequuntur! Laboriosam, tenetur eos nostrum temporibus a officia optio enim facilis quisquam consequuntur veritatis ipsa.</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 3">bouton 3</button>
+    <p v-show="sectionOpen === 3 ">loren...</p>
   </section>
 </template>
+
+
